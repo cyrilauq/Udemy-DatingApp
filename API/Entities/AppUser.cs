c77977c0f15b45cdfbc0772f6b70dc371ac7ca0e
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-24 19:51:51 +0200

    leesson 31 - updating user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,7 @@ namespace API.Entities;
 public class AppUser
 {
     public int Id { get; set; }
-    public required string UserName { get; set; } // the required modifier make our property required so it can't be null and we don't have to pass it an initial value 
+    public required string UserName { get; set; } // the required modifier make our property required so it can't be null and we don't have to pass it an initial value
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
