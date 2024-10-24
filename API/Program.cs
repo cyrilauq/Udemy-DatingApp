5a528abb46137144bae8509ca3b75cec212e79a1
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-24 11:06:29 +0200

    section 3 - lesson 22: enable cors

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,13 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200").WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
