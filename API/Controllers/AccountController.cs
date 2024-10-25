6f4b4535254169f1c99f13a0d737c202ad1406e7
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-25 12:55:07 +0200

    lesson 36: adding a login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -34,6 +34,25 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) 
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username.ToLower());
+
+        if(user is null) return Unauthorized("Invalid username");
+        
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for(int i = 0; i < computedHash.Length; i++) 
+        {
+            if(computedHash[i] != user.PasswordHash[i])  return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username) {
         return await context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower());
     }
