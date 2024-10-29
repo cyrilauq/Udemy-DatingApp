090dd627d111d8755df139c13caec4c0a5e6a53d
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-29 16:37:18 +0100

    lesson 57: hooking up the register method to the service

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
 import { HttpClient } from '@angular/common/http';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-home',
