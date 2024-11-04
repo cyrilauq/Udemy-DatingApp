c4f7eee1db80ec984f2bacff63d2502a3fa6876b
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-04 10:40:25 +0100

    lesson 90: using AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,32 @@
-using System.Collections.Generic;
-using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
         var users = await userRepository.GetUsersAsync();
 
-        return Ok(users);
+        var usersToReturn = mapper.Map<IEnumerable<AppUser>, IEnumerable<MemberDto>>(users);
+
+        return Ok(usersToReturn);
     }
 
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUserById(string username)
+    public async Task<ActionResult<MemberDto>> GetUserById(string username)
     {
         var user = await userRepository.GetUserByUsernameAsync(username);
 
         if (user is null) return NotFound();
 
-        return user;
+        return mapper.Map<AppUser, MemberDto>(user);
     }
 }
