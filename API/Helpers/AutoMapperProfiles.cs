45084db24f5fde136558d7a2457872fee7b9fac5
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-04 10:55:58 +0100

    lesson 91: configuring AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,8 @@ public class AutoMapperProfiles: Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.PhotoUrl, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)!.Url));
         CreateMap<Photo, PhotoDto>();
     }
 }
