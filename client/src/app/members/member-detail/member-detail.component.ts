38915a3e4bfab905a5c9bfeacefdc75b7d15a51f
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-05 19:48:24 +0100

    lesson 106: adding a photo gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,11 +3,13 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { Photo } from '../../_models/photo';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
 })
@@ -15,6 +17,7 @@ export class MemberDetailComponent implements OnInit {
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute); // Inject a service that will help us interact with the current route
   member?: Member;
+  images: GalleryItem[] = []
 
   ngOnInit(): void {
     this.loadMember();
@@ -24,7 +27,16 @@ export class MemberDetailComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: (member) => (this.member = member),
+      next: (member) => {
+        this.member = member;
+        this.images = member.photos.map(p => this.photoToImage(p));
+      },
+    });
+  }
+
+  private photoToImage(photo: Photo): ImageItem {
+    return new ImageItem({
+      src: photo.url, thumb: photo.url
     });
   }
 }
