0fe0f1dd86c422539f50d2be302a6d02f63b4d41
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-06 14:43:40 +0100

    lesson 113: persisting the changes in the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -8,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -26,5 +27,24 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         if (user is null) return NotFound();
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if(username == null) return BadRequest("no username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if(user == null) return BadRequest("Could not find user");
+
+        // We're updating the "user" content
+        // By doing the "map" like this, AutoMapper map the content of "memberUpdateDto" inside "user"
+        // This means it updates the "user" variable with the content of "memberUpdateDto"
+        mapper.Map(memberUpdateDto, user);
+
+        return await userRepository.SaveAllAsync() ? NoContent() : BadRequest("Failed to update the user");
     }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -13,5 +13,6 @@ public class AutoMapperProfiles: Profile
             .ForMember(d => d.Age, opt => opt.MapFrom(s => s.DateOfBirth.CalculateAge()))
             .ForMember(d => d.PhotoUrl, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)!.Url));
         CreateMap<Photo, PhotoDto>();
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
