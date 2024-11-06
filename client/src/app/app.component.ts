24116cf14c5e4a129d189eb7b66e9ab73ac53614
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-06 15:40:36 +0100

    lesson 115: adding loading indicators

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
+import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NavComponent, NgxSpinnerComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  accountService = inject(AccountService);
-  title = 'DatingApp';
-  
-  ngOnInit(): void {
-    this.setCurrentUser();
-  }
+    accountService = inject(AccountService);
+    title = 'DatingApp';
 
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if(!userString)  return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
+    ngOnInit(): void {
+        this.setCurrentUser();
+    }
+
+    setCurrentUser() {
+        const userString = localStorage.getItem('user');
+        if (!userString) return;
+        const user = JSON.parse(userString);
+        this.accountService.currentUser.set(user);
+    }
 }
