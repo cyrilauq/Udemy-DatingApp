79d3ee046b3b3c81e38bb94fc56a644a6d8b5573
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-06 16:26:11 +0100

    lesson 11: using the service to store the state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -9,16 +10,32 @@ import { Member } from '../_models/member';
 export class MembersService {
     private http = inject(HttpClient);
     baseUrl = environment.apiUrl;
+    members = signal<Member[]>([]);
 
     getMembers() {
-        return this.http.get<Member[]>(`${this.baseUrl}users`);
+        if (this.members().length == 0) {
+            this.http.get<Member[]>(`${this.baseUrl}users`).subscribe({
+                next: members => this.members.set(members),
+            });
+        }
+        return this.members();
     }
 
     getMember(username: string) {
+        const localMember = this.members().find(m => m.username === username);
+        if (localMember) return of(localMember);
         return this.http.get<Member>(`${this.baseUrl}users/${username}`);
     }
 
     updateMember(member: Member) {
-        return this.http.put(`${this.baseUrl}users`, member);
+        return this.http.put(`${this.baseUrl}users`, member).pipe(
+            tap(() => {
+                this.members.update(members =>
+                    members.map(m =>
+                        m.username === member.username ? member : m,
+                    ),
+                );
+            }),
+        );
     }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,27 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
-import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
-  selector: 'app-member-list',
-  standalone: true,
-  imports: [MemberCardComponent],
-  templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.css'
+    selector: 'app-member-list',
+    standalone: true,
+    imports: [MemberCardComponent],
+    templateUrl: './member-list.component.html',
+    styleUrl: './member-list.component.css',
 })
 export class MemberListComponent implements OnInit {
-  private membersService = inject(MembersService);
-  members: Member[] = []
+    membersService = inject(MembersService);
 
-  ngOnInit(): void {
-    this.loadMembers()
-  }
+    ngOnInit(): void {
+        this.loadMembers();
+    }
 
-  private loadMembers(): void {
-    this.membersService.getMembers().subscribe({
-      next: members => this.members = members
-    })
-  }
-
+    private loadMembers(): void {
+        this.membersService.getMembers();
+    }
 }
