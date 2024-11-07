00e37536bc793b3ccce26555f80f558beae6b2b7
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-07 19:45:19 +0100

    lesson 128: adding a photo uploader part two

--- client/src/app/member/photo-editor/photo-editor.component.ts
+++ client/src/app/member/photo-editor/photo-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
 import { Member } from '../../_models/member';
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
@@ -8,7 +8,7 @@ import { environment } from '../../../environments/environment';
 @Component({
     selector: 'app-photo-editor',
     standalone: true,
-    imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule],
+    imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule, DecimalPipe],
     templateUrl: './photo-editor.component.html',
     styleUrl: './photo-editor.component.css',
 })
