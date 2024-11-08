6e0d6bb1f6d0678cf4fb986978462de47de549a1
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-08 10:42:36 +0100

    leesson 129: setting the main photo in the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -34,9 +34,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.GetUsername();
-
-        var user = await userRepository.GetUserByUsernameAsync(username);
+        var user = await GetConnectedUser(userRepository);
 
         if (user == null) return BadRequest("Could not find user");
 
@@ -51,7 +49,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
     [HttpPost("add-photo")]
     public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
     {
-        var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+        var user = await GetConnectedUser(userRepository);
 
         if (user == null) return BadRequest("Can't update user");
 
@@ -68,5 +66,29 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
         user.Photos.Add(photo);
 
         return await userRepository.SaveAllAsync() ? CreatedAtAction(nameof(GetUser), new { username = user.UserName }, mapper.Map<PhotoDto>(photo)) : BadRequest("Problem adding photo");
+    }
+
+    [HttpPut("set-main-photo/{photoId:int}")]
+    public async Task<ActionResult> SetMainPhoto(int photoId)
+    {
+        var user = await GetConnectedUser(userRepository);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        var photo = user.Photos.FirstOrDefault(p => p.Id == photoId);
+
+        if (photo == null) return BadRequest($"No photo found for the id {photoId}");
+        if (photo.IsMain) return BadRequest($"Photo with id '{photoId}' is already the main photo");
+
+        var currentMain = user.Photos.FirstOrDefault(p => p.IsMain);
+        if (currentMain != null) currentMain.IsMain = false;
+        photo.IsMain = true;
+
+        return await userRepository.SaveAllAsync() ? NoContent() : BadRequest("A problem occure while setting the main photo");
+    }
+
+    private Task<AppUser?> GetConnectedUser(IUserRepository userRepository)
+    {
+        return userRepository.GetUserByUsernameAsync(User.GetUsername());
     }
 }
