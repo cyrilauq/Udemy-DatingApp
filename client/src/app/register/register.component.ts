5c004156366a2ab7128e89f515db958c2c04887a
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-09 15:07:53 +0100

    lesson 137: client side validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, JsonPipe],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css',
 })
@@ -24,14 +30,17 @@ export class RegisterComponent implements OnInit {
 
     initializeForm() {
         this.registerForm = new FormGroup({
-            username: new FormControl(),
-            password: new FormControl(),
-            confirmPassword: new FormControl(),
+            username: new FormControl('', Validators.required),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(24),
+            ]),
+            confirmPassword: new FormControl('', [Validators.required]),
         });
     }
 
     register() {
-        console.log(this.registerForm.value);
         // this.accountService.register(this.model).subscribe({
         //     next: response => {
         //         console.log(response);
