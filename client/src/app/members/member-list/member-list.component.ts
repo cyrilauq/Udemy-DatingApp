37f645dec7ce23c83ce96fb3962c09fa1c375782
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-11 15:43:08 +0100

    lesson 152: adding the angular bootstrap pagination component

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from '../member-card/member-card.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
     selector: 'app-member-list',
     standalone: true,
-    imports: [MemberCardComponent],
+    imports: [MemberCardComponent, PaginationModule],
     templateUrl: './member-list.component.html',
     styleUrl: './member-list.component.css',
 })
@@ -20,5 +21,12 @@ export class MemberListComponent implements OnInit {
 
     private loadMembers(): void {
         this.membersService.getMembers(this.pageNumber, this.pageSize);
+    }
+
+    onPageChanged(event: any) {
+        if (this.pageNumber != event.page) {
+            this.pageNumber = event.page;
+            this.loadMembers();
+        }
     }
 }
