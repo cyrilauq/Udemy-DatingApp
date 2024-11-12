0d87cffa6c9eb5cbba8988189e7d9b0ddb8b0e18
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-12 08:53:18 +0100

    lesson 156: adding filter buttons to the client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -4,25 +4,33 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { UserParams } from '../../_models/userParams';
 import { AccountService } from '../../_services/account.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-member-list',
     standalone: true,
-    imports: [MemberCardComponent, PaginationModule],
+    imports: [MemberCardComponent, PaginationModule, FormsModule],
     templateUrl: './member-list.component.html',
     styleUrl: './member-list.component.css',
 })
 export class MemberListComponent implements OnInit {
     membersService = inject(MembersService);
     private accountService = inject(AccountService);
+
     userParams = new UserParams(this.accountService.currentUser());
+    genderList = [{ value: 'male', display: "Males" }, { value: 'female', display: 'Females' }];
 
     ngOnInit(): void {
         this.loadMembers();
     }
 
-    private loadMembers(): void {
+    loadMembers(): void {
         this.membersService.getMembers(this.userParams);
+    }
+
+    resetFilters() {
+        this.userParams = new UserParams(this.accountService.currentUser());
+        this.loadMembers();
     }
 
     onPageChanged(event: any) {
