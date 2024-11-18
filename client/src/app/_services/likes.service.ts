c5ded1a0819c4a0ee2d3cd1776b540aae55e371e
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-18 16:07:05 +0100

    lesson 173: adding the lists component

--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,15 @@ export class LikesService {
   likeIds = signal<number[]>([]);
   
   toggleLike(targetId: number) {
-    return this.http.post(`${this.baseUrl}likes/${targetId}`, {});
+    return this.http.post(`${this.baseUrl}likes/${targetId}`, {}).pipe(
+      tap(() => {
+        if(this.likeIds().includes(targetId)) {
+          this.likeIds.update(ids => ids.filter(id => id !== targetId))
+        } else {
+          this.likeIds.update(ids => [...ids, targetId])
+        }
+      })
+    );
   }
 
   getLikes(predicate: string) {
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
 import { LikesService } from '../../_services/likes.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-card',
@@ -12,6 +13,15 @@ import { LikesService } from '../../_services/likes.service';
 })
 export class MemberCardComponent {
   private likesService = inject(LikesService);
+  private toastrService = inject(ToastrService)
   member = input.required<Member>();
   hasLiked = computed(() => this.likesService.likeIds().includes(this.member().id));
+
+  toggleLike() {
+    this.likesService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        this.toastrService.success(this.hasLiked() ? "User liked" : "User disliked")
+      }
+    })
+  }
 }
