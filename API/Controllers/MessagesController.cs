819f4bd96092767463614d4b6c0026326570ccfe
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-20 12:08:46 +0100

    lesson 182: getting the messages from the repository

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -2,6 +2,7 @@ using System;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -42,5 +43,17 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
             RecipientUsername = recipient.UserName,
             Content = createMessageDto.Content
         };
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+    {
+        messageParams.Username = User.GetUsername();
+
+        var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+        Response.AddPaginationHeader(messages);
+
+        return messages;
     }
 }
