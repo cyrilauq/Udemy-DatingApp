d711e040573a7402be2a4ea87d3bec1d09b2aa47
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-20 15:42:14 +0100

    lesson 184: setting up the angular app for messaging

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessageService } from '../_services/message.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
-  selector: 'app-messages',
-  standalone: true,
-  imports: [],
-  templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+    selector: 'app-messages',
+    standalone: true,
+    imports: [ButtonsModule],
+    templateUrl: './messages.component.html',
+    styleUrl: './messages.component.css',
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+    messagesService = inject(MessageService);
+    container = 'Inbox';
+    pageNumber = 1;
+    pageSize = 5;
 
+    ngOnInit(): void {
+        this.loadMessages();
+    }
+
+    loadMessages() {
+        this.messagesService.getMessages(
+            this.pageNumber,
+            this.pageSize,
+            this.container,
+        );
+    }
+
+    onPageChanged(event: any) {
+        if (event.page != this.pageNumber) {
+            this.pageNumber = event.page;
+            this.loadMessages();
+        }
+    }
 }
