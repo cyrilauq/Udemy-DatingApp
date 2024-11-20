d82b95bf8d17bf798bafb8a6f3b44fa7e13e7663
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-20 12:38:07 +0100

    lesson 183: getting the message thread for 2 users

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -5,6 +5,7 @@ using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
@@ -43,9 +44,27 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
         return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUserName)
+    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUserName)
     {
-        throw new NotImplementedException();
+        var messages = await context.Messages
+            .Include(m => m.Sender).ThenInclude(u => u.Photos)
+            .Include(m => m.Recipient).ThenInclude(u => u.Photos)
+            .Where(m => (m.RecipientUsername == currentUsername && m.SenderUsername == recipientUserName) ||
+                (m.SenderUsername == currentUsername && m.RecipientUsername == recipientUserName))
+            .OrderBy(m => m.MessageSent)
+            .ToListAsync();
+
+        var unreadMessages = messages
+            .Where(m => m.DateRead == null && m.RecipientUsername == currentUsername)
+            .ToList();
+
+        if(unreadMessages.Any())
+        {
+            unreadMessages.ForEach(m => m.DateRead = DateTime.UtcNow);
+            await context.SaveChangesAsync();
+        }
+        
+        return mapper.Map<IEnumerable<Message>, IEnumerable<MessageDto>>(messages);
     }
 
     public async Task<bool> SaveAllAsync()
