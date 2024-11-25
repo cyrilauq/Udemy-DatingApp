2f6458ed45ebfe186a944b805aacdbec2b631d13
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-11-25 12:07:29 +0100

    lesson 188: activating the message tab

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,45 +1,67 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { Photo } from '../../_models/photo';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
-import { MemberMessagesComponent } from "../member-messages/member-messages.component";
+import { MemberMessagesComponent } from '../member-messages/member-messages.component';
+import { Message } from '../../_models/message';
+import { MessageService } from '../../_services/message.service';
 
 @Component({
-  selector: 'app-member-detail',
-  standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
-  templateUrl: './member-detail.component.html',
-  styleUrl: './member-detail.component.css',
+    selector: 'app-member-detail',
+    standalone: true,
+    imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
+    templateUrl: './member-detail.component.html',
+    styleUrl: './member-detail.component.css',
 })
 export class MemberDetailComponent implements OnInit {
-  private memberService = inject(MembersService);
-  private route = inject(ActivatedRoute); // Inject a service that will help us interact with the current route
-  member?: Member;
-  images: GalleryItem[] = []
+    @ViewChild('memberTabs') memberTabs?: TabsetComponent;
 
-  ngOnInit(): void {
-    this.loadMember();
-  }
+    private memberService = inject(MembersService);
+    private messageService = inject(MessageService);
+    private route = inject(ActivatedRoute); // Inject a service that will help us interact with the current route
 
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: (member) => {
-        this.member = member;
-        this.images = member.photos.map(p => this.photoToImage(p));
-      },
-    });
-  }
+    member?: Member;
+    images: GalleryItem[] = [];
+    activeTab?: TabDirective;
+    messages: Message[] = [];
 
-  private photoToImage(photo: Photo): ImageItem {
-    return new ImageItem({
-      src: photo.url, thumb: photo.url
-    });
-  }
+    ngOnInit(): void {
+        this.loadMember();
+    }
+
+    onTabActivated(data: TabDirective) {
+        this.activeTab = data;
+        if (this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member) {
+            this.loadMessages();
+        }
+    }
+
+    loadMessages() {
+        this.messageService.getMessageThread(this.member!.username).subscribe({
+            next: messages => (this.messages = messages),
+        });
+    }
+
+    loadMember() {
+        const username = this.route.snapshot.paramMap.get('username');
+        if (!username) return;
+        this.memberService.getMember(username).subscribe({
+            next: member => {
+                this.member = member;
+                this.images = member.photos.map(p => this.photoToImage(p));
+            },
+        });
+    }
+
+    private photoToImage(photo: Photo): ImageItem {
+        return new ImageItem({
+            src: photo.url,
+            thumb: photo.url,
+        });
+    }
 }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,28 +1,18 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 
 @Component({
-  selector: 'app-member-messages',
-  standalone: true,
-  imports: [TimeagoModule],
-  templateUrl: './member-messages.component.html',
-  styleUrl: './member-messages.component.css'
+    selector: 'app-member-messages',
+    standalone: true,
+    imports: [TimeagoModule],
+    templateUrl: './member-messages.component.html',
+    styleUrl: './member-messages.component.css',
 })
-export class MemberMessagesComponent implements OnInit {
-  private messageService = inject(MessageService);
-  
-  username = input.required<string>();
-  messages: Message[] = [];
+export class MemberMessagesComponent {
+    private messageService = inject(MessageService);
 
-  ngOnInit(): void {
-    this.loadMessages();
-  }
-
-  loadMessages() {
-    this.messageService.getMessageThread(this.username()).subscribe({
-      next: messages => this.messages= messages
-    })
-  }
+    username = input.required<string>();
+    messages = input.required<Message[]>();
 }
