e0b1ece3c6bced462c0bf8a110a340b9aee022b4
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-02 11:50:12 +0100

    lesson 203: adding roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,19 +9,61 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    private static JsonSerializerOptions _jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
-        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+        await AddRolesToManagerAsync(roleManager, GetRoles());
 
-        var jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, jsonOptions) ?? [];
+        await AddUsersToManagerAsync(userManager, await GetUsersAsync());
+    }
+
+    private static IEnumerable<AppRole> GetRoles()
+    {
+        return new List<AppRole>
+        {
+            new() { Name = "Member" },
+            new() { Name = "Admin" },
+            new() { Name = "Moderator" }
+        };
+    }
+
+    private static async Task AddRolesToManagerAsync(RoleManager<AppRole> roleManager, IEnumerable<AppRole> roles)
+    {
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+    }
+
+    private static async Task AddUsersToManagerAsync(UserManager<AppUser> userManager, IEnumerable<AppUser> users)
+    {
 
         foreach (var user in users)
         {
             user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser
+        {
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = "",
+            City = "",
+            Country = ""
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, [ "Admin", "Moderator" ]);
+    }
+
+    private static async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+        return JsonSerializer.Deserialize<List<AppUser>>(userData, _jsonOptions) ?? [];
     }
 }
