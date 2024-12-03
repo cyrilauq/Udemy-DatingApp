1554f43b2f870edee3a2ecb5a9e11c5a936ad9a9
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-03 10:43:33 +0100

    lesson 206: getting the users with roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,21 +1,32 @@
-using System;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users")]
-    public ActionResult GetUsersWithRoles()
+    public async Task<ActionResult> GetUsersWithRoles()
     {
-        return Ok("Only admins can see this");
+        var users = await userManager.Users
+            .OrderBy(u => u.UserName)
+            .Select(u => new
+            {
+                u.Id,
+                Username = u.UserName,
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            })
+            .ToListAsync();
+        return Ok(users);
     }
     
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("moderate")]
-    public ActionResult GetGetPhotosForModeration()
+    public async Task<ActionResult> GetGetPhotosForModeration()
     {
         return Ok("Only admins can see this");
     }
