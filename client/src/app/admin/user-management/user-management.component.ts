0f8a683d41a779df9a321314d21c9c8d30d307ee
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-11 09:13:54 +0100

    lesson 212: setting up modals

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AdminService } from '../../_services/admin.service';
 import { User } from '../../_models/User';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.component';
 
 @Component({
     selector: 'app-user-management',
@@ -11,11 +13,26 @@ import { User } from '../../_models/User';
 })
 export class UserManagementComponent implements OnInit {
     users: User[] = [];
+    // We need to tells bootstrap the component that will be used as a modal and that's why we type it with RolesModalComponent
+    bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
     private adminservice = inject(AdminService);
+    private modalService = inject(BsModalService);
 
     ngOnInit(): void {
         this.getUsersWithRoles();
+    }
+
+    openRolesModal() {
+        const modalOptions: ModalOptions = {
+            class: 'modal-lg',
+            initialState: {
+                title: 'User roles',
+                list: ['Admin', 'Moderator', 'Member']
+            }
+        };
+
+        this.bsModalRef = this.modalService.show(RolesModalComponent, modalOptions);
     }
 
     getUsersWithRoles() {
