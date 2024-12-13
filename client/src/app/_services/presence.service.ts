878610e08b17749771431fac09275131b6c63504
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-13 10:19:30 +0100

    lesson 219: client side signalr

---
+++ client/src/app/_services/presence.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { User } from '../_models/User';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PresenceService {
+    private hubUrl = environment.hubsUrl;
+    private hubConnection?: HubConnection;
+    private toastrService = inject(ToastrService);
+
+    createHubConnection(user: User) {
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl(`${this.hubUrl}presence`, {
+                accessTokenFactory: () => user.token
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        this.hubConnection.start().catch(error => console.error(error));
+
+        this.hubConnection.on('UserIsOnline', username => {
+            this.toastrService.info(`${username} has connected`)
+        });
+        this.hubConnection.on('UserIsOffline', username => {
+            this.toastrService.warning(`${username} has disconnected`)
+        });
+    }
+
+    stopHubConnection() {
+        if(this.hubConnection?.state === HubConnectionState.Connected) {
+            this.hubConnection.stop().catch(error => console.error(error));
+        }
+    }
+}
