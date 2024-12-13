63c956b4bced2849b7ea79a9a49c435daa5f61d3
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-12-13 16:52:13 +0100

    lesson 223: adding the send message method to the hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,11 +1,14 @@
 using System;
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR;
 
-public class MessageHub(IMessageRepository messageRepository) : Hub
+public class MessageHub(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper) : Hub
 {
     public override async Task OnConnectedAsync()
     {
@@ -25,6 +28,43 @@ public class MessageHub(IMessageRepository messageRepository) : Hub
     public override Task OnDisconnectedAsync(Exception? exception)
     {
         return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+        var username = Context.User?.GetUsername() ?? throw new Exception("Could not get user");
+        var recipientUsername = createMessageDto.RecipientUsername.ToLower();
+
+        if(username == recipientUsername)
+        {
+            throw new HubException("You can't send a message to yourself");
+        }
+
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(recipientUsername);
+
+        if(recipient == null || sender == null || recipient.UserName == null || sender.UserName == null) throw new HubException("Can' send a message at this time");
+        
+        var message = CreateMessageFrom(sender, recipient, createMessageDto);
+        messageRepository.AddMessage(message);
+
+        if(await messageRepository.SaveAllAsync())
+        {
+            var groupName = GetGroupName(sender.UserName, recipient.UserName);
+            await Clients.Group(groupName).SendAsync("NewMessage", mapper.Map<MessageDto>(message));
+        }
+    }
+
+    private Message CreateMessageFrom(AppUser sender, AppUser recipient, CreateMessageDto createMessageDto)
+    {
+        return new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName!,
+            RecipientUsername = recipient.UserName!,
+            Content = createMessageDto.Content
+        };
     }
 
     private string GetGroupName(string caller, string other)
